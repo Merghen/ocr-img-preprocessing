8f1c18d9a543c64bb5011bed826b9cffe9011a99
Omer Faruk <faruktopsakal3@gmail.com>
2025-07-14 12:39:28 +0300

    fixed missing files

--- image to text/obj/Debug/net8.0-windows/image to text.AssemblyInfo.cs
+++ image to text/obj/Debug/net8.0-windows/image to text.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("image to text")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+501f0f56c9d948c0f3de232c583c9339dea8b574")]
 [assembly: System.Reflection.AssemblyProductAttribute("image to text")]
 [assembly: System.Reflection.AssemblyTitleAttribute("image to text")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
